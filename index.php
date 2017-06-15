86170119dc37ca49a445f05be112e97bf9dedc21
Schuyler Ankele <schuyler.ankele@gmail.com>
2017-06-14 23:40:42 -0600

    Form to hold our keys

--- index.php
+++ index.php
@@ -6,7 +6,22 @@
     
 </head>
 <body>
-
+<form id="plugin-settings">
+    <div class="form-group">
+        <label for="url" >Website URL:<span>*</span></label>
+        <input type="text" placeholder="http://www.example.com" class="form-control" name="url" id="url" required="required" aria-required="true">
+    </div>
+    <div class="form-group">
+        <label for="ck" class="fb-text-label">WooCommerce Consumer Key</label>
+        <input type="text" class="form-control" name="ck" id="ck"></div>
+    <div class="form-group">
+        <label for="cs">WooCommerce Consumer Secret</label>
+        <input type="text" class="form-control" name="cs" id="cs">
+    </div>
+    <div class="form-group">
+        <button type="button" class="btn btn-primary" name="savekey" style="primary" id="savekey" onclick="postVals()">Save</button>
+    </div>
+</form>
 <?php
 
 
