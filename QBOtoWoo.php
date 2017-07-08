c531f6d65477655f2f39f827207003fcc6a41214
Schuyler Ankele <schuyler.ankele@gmail.com>
2017-07-07 23:32:19 -0600

    Refactoring the interface to separate files

--- QBOtoWoo.php
+++ QBOtoWoo.php
@@ -1,19 +1,93 @@
 <?php
 
+/**
+ * @author Schuyler Ankele
+ * @author Schuyler Ankele <schuyler.ankele@gmail.com>
+ *
+ * Class QBOtoWoo - this is a collection of functions for QBOtoWoo
+ *
+ * future improvements could be variables to store products changed and a log to rollback mistakes.
+ *
+ */
+
+
+
+
 class QBOtoWoo{
-    
-    
-    // Check if install has been completed
-    
+    /**
+     *  Check if install has been completed
+     *  if the file has not been created then we cannot run
+     *  This will be replaced by DB vals
+     */
+    private $installed = false;
+
     function checkInit(){
         $installed = file_exists("file.ini");
         return $installed;
     }
-    
-    
-    function WooConnect(){
-        
-    } 
+
+    /**
+     * @param $product - is a single product object from WooCommerce
+     * @param bool $keys_only - Optional: Will return an array of Product Keys with no values
+     * This function is built for WooCommerce REST API V2 Objects
+     *
+     */
+    function getProdAttributes($product, $keys_only = false){
+        foreach ($product as $key => $value) {
+            // Check for multidimensions
+            if(is_object($value) || is_array($value)){
+                // To break down an object
+                if(is_object($value)){
+                    echo 'Object Name:' . $key . ' =  <br>';
+                    array_push($productKeys, $key);
+                    echo 'Pushed to Array';
+                    foreach($value as $objKey => $attr){
+                        echo 'Object Attribute: ' . $objKey . '<br>' . $attr;
+                    }
+                    var_dump($value);
+                }
+                // If its an array
+                else{
+                    echo '<p>Array Name:' . $key;
+                    array_push($productKeys, $key);
+                    echo '<b>Pushed to Array</b></p>';
+                    print_r($value);
+
+                    // to break down the Array
+                    foreach($value as $subkey => $subval){
+                        if(is_object($subval)){
+                            echo 'This subvalue is an Object';
+                            echo $subkey;
+                        }
+                        /*
+                        echo '<p>Subkey</p>';
+                        print_r($subkey);
+                        echo '<p>SubVal</p>';
+                        print_r($subval);
+                        */
+
+                    }
+
+                }
+
+
+            }
+        // array_push($quickArray, $value);
+
+        // var_dump($quickArray);
+
+
+            else {
+                echo '<p>' . $key . ' = ' . $value;
+                array_push($productKeys, $key);
+                echo ' <b>*Pushed to Array</b></p>';
+            }
+
+        }
+        // var_dump($rescueArr);
+        echo '<h1>Product Attributes</h1>';
+        print_r($productKeys);
+    }
     
     
 }
--- WooProdTable.php
+++ WooProdTable.php
@@ -2,9 +2,8 @@
 /**
  * WooProdTable.php -
  * I need to see the data I am modeling in order to know how I am
- * User: Shoe
- * Date: 6/23/2017
- * Time: 10:30 PM
+ * @author: Shoe
+ *
  */
     include 'WooProducts.php';
     include 'WooConnect.php';
@@ -14,10 +13,11 @@
 
             $woo_connection->loadSettings();
             $woo_connection->storeConnect();
-            // print_r($connection->getFromWoo('products/attributes'));
+
+            // We will pass the full data object into JS and parse it down
             $JSONres = $woo_connection->getFromWoo('products');
-            $productKeys = array();
-            // var_dump($JSONres);
+
+            var_dump($JSONres);
             $rescueArr = json_decode($JSONres);
             $quickArray = array();
 
@@ -37,20 +37,113 @@
     <title>Woo Commerce Product</title>
     
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
-    
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
 </head>
 <body>
-<div id="QBOtoWoo_Products">
-    <?php echo $JSONres; ?>
+<div id="QBOtoWoo_Products"></div>
+<div class="container">
+    <div class="row">
+        <div id="test" class="col-8"></div>
+        <div id="test2" class="col-4"></div>
+
+    </div>
+
+
+
 </div>
 
 <script type="application/ecmascript">
+    // bs4Table is a simple JS function to create tables from object arrays
+
+    function bs4Table(an_object_array, targetDiv, visibilityToggle){
+
+        const optionsDiv = document.querySelector(visibilityToggle);
+        const mainDiv = document.querySelector(targetDiv);
+        // Create a table
+        const aTable = document.createElement('table');
+        aTable.setAttribute('class', 'table');
+        // The header
+        const header = aTable.createTHead();
+        header.setAttribute('class', 'thead-inverse');
+        var attrList = [];
+        th = header.insertRow(0);
+        th.setAttribute('id', 'productHeading');
+        var id_th = th.insertCell(0);
+        id_th.innerHTML = 'ID';
+        var name_th = th.insertCell(1);
+        name_th.innerHTML = 'Name';
+        var sku_th = th.insertCell(2);
+        sku_th.innerHTML = 'SKU';
+        aTable.appendChild(th);
+
+        // Check if an products are created
+        for (var [key, value] of Object.entries(an_object_array[0])) {
+            //console.log(key + ' ' + value); // Will Return our list of attributes and table headings
+            attrList.push(key);
+        }
+        if(visibilityToggle.length > 1){
+            try{
+                // Build the options pane for selecting which cols are visible
+                const visToggler = document.createElement('table');
+                visToggler.setAttribute('class', 'table');
+                const visHeader = visToggler.createTHead();
+                visHeader.setAttribute('class', 'thead-inverse');
+                vTH = visHeader.insertRow(0);
+                attrCol = vTH.insertCell(0);
+                attrCol.innerHTML = 'Attribute';
+                visSelCol = vTH.insertCell(1);
+                visSelCol.innerHTML = 'Visible?';
+
+                // Options pane - we should limit this selection size to 8 or bad things will happen
+                // Fill the pane....heh
+                attrList.forEach(function (element) {
+                    _attrRow = document.createElement('tr');
+                    _attrName = _attrRow.insertCell(0);
+                    _attrName.innerHTML = element;
+                    _attrVis = _attrRow.insertCell(1);
+                    if(element == 'id' || element == 'name')
+                    {_attrVis.innerHTML = '<b>Required</b>';}
+                    else {_attrVis.innerHTML =
+                        '<input type="checkbox" id="' + element + 'Visibility" />';}
+                    visToggler.appendChild(_attrRow);
+                });
+                optionsDiv.appendChild(visToggler);
+            }
+            catch (e){
+                alert(e.message);
+            }
+        }
+        console.log(attrList);
+
+        an_object_array.forEach(function (element) {
+            tr = document.createElement('tr');
+            _id = tr.insertCell(0);
+            _id.innerHTML = element.id;
+            _name = tr.insertCell(1);
+            _name.innerHTML = element.name;
+            aTable.appendChild(tr);
+        });
+
+
+
+
+        mainDiv.appendChild(aTable);
+    }
+</script>
+
+
+
+
+<script type="application/ecmascript">
+    // Attempt 1
+
     var prod = new Object();
     prod = 
         <?php echo $JSONres?>;
+    bs4Table(prod, '#test', '#test2');
     const content = document.getElementById('QBOtoWoo_Products');
     const table = document.createElement('table');
     const header = document.createElement('th');
@@ -91,15 +184,8 @@
         var price = singleProd.insertCell(5);
         price.innerHTML = obj.price;
         var mng_qty = singleProd.insertCell(6);
-        short_desc.innerHTML = obj.manage_stock;
-        
-        
-    })
+        short_desc.innerHTML = obj.manage_stock;});
     
-    class WooTables{
-        
-    }
-
 
 </script>
 <script type="application/javascript">
